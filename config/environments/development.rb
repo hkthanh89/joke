f9acd86bd638f95b6bdb03364463b542eeb53678
Huynh Kien Thanh <hkthanh89@thanh-mac-mini.local>
2016-03-28 04:17:13 +0700

    Config action mailer to send emails using Mailgun service

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,12 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.smtp_settings = {
+    address: ENV["SMTP_ADDRESS"],
+    port: ENV["SMTP_PORT"],
+    domain: ENV["SMTP_DOMAIN"],
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: ENV["SMTP_AUTHENTICATION"]
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: ENV["HOST_NAME"] }
+  config.action_mailer.smtp_settings = {
+    address: ENV["SMTP_ADDRESS"],
+    port: ENV["SMTP_PORT"],
+    domain: ENV["SMTP_DOMAIN"],
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: ENV["SMTP_AUTHENTICATION"]
+  }
 end
