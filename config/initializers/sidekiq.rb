410554d9b1ebb5dc0e663ca6d659f836a4dbf4b9
Huynh Kien Thanh <hkthanh89@thanh-mac-mini.local>
2016-03-31 13:53:55 +0700

    Set Redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,16 @@
+def set_redis_config
+  if Rails.env.production?
+    { url: 'redis://localhost:6379/12', namespace: "jokee" }
+  else
+    { url: 'redis://localhost:6379/12', namespace: "jokee" }
+  end
+end
+
 Sidekiq.configure_server do |config|
   config.failures_max_count = 2000
+  config.redis = set_redis_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = set_redis_config
 end
